070c63604d8027f7bf4562dd079e1f3bec96dc38
turaevakani <kanykeituraeva03@gmail.com>
2022-10-05 22:05:52 +0600

    hi

--- src/Main.java
+++ src/Main.java
@@ -1,34 +1,33 @@
 import java.util.Random;
-import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-                System.out.println( valueOfTem (30,-18 ));
-                System.out.println( valueOfTem (5,21 ));
-                System.out.println( valueOfTem (19,33 ));
-                System.out.println( valueOfTem (65,-5 ));
-                System.out.println( valueOfTem (2,-21 ));
+        System.out.println(valueOfTem(30, -18));
+        System.out.println(valueOfTem(5, 21));
+        System.out.println(valueOfTem(19, 33));
+        System.out.println(valueOfTem(65, -5));
+        System.out.println(valueOfTem(2, -21));
+        System.out.println(valueOfTem(generateRandomAge(),12));
 
-            }
+    }
 
-            public static String valueOfTem( int age, int temperature){
-                if (age>=20 && age<=45 && temperature>=-20 && temperature<=30) {
-                    return "Можно идти гулять";
-                }
-                else if (age<=20  && temperature>=0 && temperature<=28){
-                    return "Можно идти гулять";
-                }
-                else if (age>=45  && temperature>=-10 && temperature<=45){
-                    return "Можно идти гулять";
-                }
-                else{
-                    return "Оставайтесь дома";
-                }
+    public static String valueOfTem(int age, int temperature) {
+        if (age >= 20 && age <= 45 && temperature >= -20 && temperature <= 30) {
+            return "Можно идти гулять";
+        } else if (age <= 20 && temperature >= 0 && temperature <= 28) {
+            return "Можно идти гулять";
+        } else if (age >= 45 && temperature >= -10 && temperature <= 45) {
+            return "Можно идти гулять";
+        } else {
+            return "Оставайтесь дома";
+        }
 
-            }
+    }
+
     public static int generateRandomAge(){
-        Random r = new Random();
-        int num = r.nextInt();
-        System.out.println("random int is: " + num);
+        Random r =new Random();
+        int num = r.nextInt(100)+1;
+        System.out.println("random int is: "+num);
         return num;
     }
 
